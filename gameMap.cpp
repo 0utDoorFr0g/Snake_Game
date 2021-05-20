e1f65c56e72688d4b82b9f2eee527c64648b39ef
소프트웨어학부 문원기 <39542985+moonwalk99@users.noreply.github.com>
2021-05-21 03:38:13 +0900

    snake 구현 및 게임맵 인터페이스 오류 수정, 그리고 Position.cpp 추가
    gameWindow.cpp의 85번째 줄부터 시작하는 주석문을 잘 읽을 것

--- gameMap.cpp
+++ gameMap.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <vector>
 #include "gameMap.h"
+
 
 using namespace std;
 
@@ -10,6 +12,7 @@ gameMap::gameMap()
   생성자가 호출되면 기본으로 currentMap을 map[0]으로 바꾼다.
   */
   setCurrentMap(0);
+  snakeInitialize();
 }
 
 int gameMap::getCurrentMapPosition(int x, int y)
@@ -31,4 +34,41 @@ void gameMap::setCurrentMap(int num)
       currentMap[i][j] = map[num][i][j];
     }
   }
+}
+
+void gameMap::snakeInitialize()
+{
+  currentMap[snakeOnMap.getSnakePosY(0)][snakeOnMap.getSnakePosX(0)] = 3;
+  for (int i = 1; i < snakeOnMap.getSnakeLength(); i++)
+  {
+    currentMap[snakeOnMap.getSnakePosY(i)][snakeOnMap.getSnakePosX(i)] = 4;
+  }
+}
+
+void gameMap::gameTimeFlow()
+{
+  //tempX와 tempY는 snake의 마지막 꼬리의 좌표로 만약 아이템 획득 없이 snake가 전진한다면 이 좌표에 해당하는 currentMap을 0으로 바꿔줘야 한다.
+  int tempX = snakeOnMap.getSnakePosX(snakeOnMap.getSnakeLength()-1);
+  int tempY = snakeOnMap.getSnakePosY(snakeOnMap.getSnakeLength()-1);
+  //snake 전진
+  snakeOnMap.snakeGo();
+
+  if (currentMap[snakeOnMap.getSnakePosY(0)][snakeOnMap.getSnakePosX(0)] != 0)
+  {
+    //아이템 획득 or 벽 부딪힘.
+  }
+  else
+  { //머리 부분 currentMap 반영
+    currentMap[snakeOnMap.getSnakePosY(0)][snakeOnMap.getSnakePosX(0)] = 3;
+    //몸통과 꼬리 currentMap 반영
+    for (int i = 1; i < snakeOnMap.getSnakeLength(); i++)
+    {
+      currentMap[snakeOnMap.getSnakePosY(i)][snakeOnMap.getSnakePosX(i)] = 4;
+    }
+    //꼬리부분 0으로 바꾸기
+    currentMap[tempY][tempX] = 0;
+  }
+
+
+
 }
