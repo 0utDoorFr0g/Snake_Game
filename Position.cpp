c0bf48a601eb486f630d803515d0d4bc8fc651f2
0utDoorFr0g <outdoorfrog@kookmin.ac.kr>
2021-05-23 15:54:50 +0900

    step 1
    Moon-branch Upload

---
+++ Position.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Position.h"
+
+using namespace std;
+
+Position::Position(int py, int px, int ptype): x(px), y(py), type(ptype){}
+
+int Position::getPosX()
+{
+  return this->x;
+}
+int Position::getPosY()
+{
+  return this->y;
+}
+int Position::getType()
+{
+  return type;
+}
+
+void Position::setPosX(int px)
+{
+  this->x = px;
+}
+void Position::setPosY(int py)
+{
+  this->y = py;
+}
+void Position::setType(int ptype)
+{
+  this->type = ptype;
+}
