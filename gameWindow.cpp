602f2c2c168952f3fbbbffdb85fcd74a69a030a8
소프트웨어학부 문원기 <39542985+moonwalk99@users.noreply.github.com>
2021-05-24 22:25:46 +0900

    snake 발자취 남기는 오류 해결

--- gameWindow.cpp
+++ gameWindow.cpp
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <ctime>
 #include "gameMap.h"
+#include "kbtick.h"
 
 using namespace std;
 
@@ -40,26 +41,29 @@ int main()
 
   //map 표시 시작
   //0은 빈공간(\u2B1C), 1은 wall, 2는 immune wall (\u2B1B), 3은 snake 머리, 4는 몸통 (\u2B1A)
-  for (int i = 0; i < 21; i++)
+  int emptySquare;
+  for (int h = 0; h < 21; h++)
   {
-    for (int j = 0; j < 21; j++)
+    for (int w = 0; w < 21; w++)
     {
-      if(mapControl.getCurrentMapPosition(i,j) == 0)
+      if(mapControl.getCurrentMapPosition(h,w) == 0)
       {
-        mvwprintw(gameScreen,j,2*i," ");
+        mvwprintw(gameScreen,h,2*w," ");
       }
-      else if(mapControl.getCurrentMapPosition(i,j) == 1 || mapControl.getCurrentMapPosition(i,j) == 2)
+      else if(mapControl.getCurrentMapPosition(h,w) == 1 || mapControl.getCurrentMapPosition(h,w) == 2)
       {
-        mvwprintw(gameScreen,j,2*i,"\u25A1");
+        mvwprintw(gameScreen,h,2*w,"\u25A1");
       }
-      else if(mapControl.getCurrentMapPosition(i,j) == 3)
+      else if(mapControl.getCurrentMapPosition(h,w) == 3)
       {
-        mvwprintw(gameScreen,j,2*i,"\u25CC");
+        mvwprintw(gameScreen,h,2*w,"\u25CC");
       }
-      else if(mapControl.getCurrentMapPosition(i,j) == 4)
+      else if(mapControl.getCurrentMapPosition(h,w) == 4)
       {
-        mvwprintw(gameScreen,j,2*i,"\u25CB");
+        mvwprintw(gameScreen,h,2*w,"\u25CB");
       }
+      emptySquare = 2*w - 1;
+      mvwprintw(gameScreen,h,emptySquare," ");
     }
   }
   wrefresh(gameScreen);
@@ -85,33 +89,45 @@ int main()
   //중요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
   //시간 틱이 반복문을 통해 수행한다는 가정하에 단지 gameTimeFlow를 실행하는 것으로 map의 데이터를 바꿀 수 있다!!!
   //아이템에 부딪히는 것과 벽과 부딪히는 것은 아직 실패 스크린을 구상하지 않았기에 비어있는 if문으로 처리를 했다.
-  getch();
-  mapControl.gameTimeFlow();
-  mapControl.snakeChangeDirection(3);
-  mapControl.gameTimeFlow();
-  for (int i = 0; i < 21; i++)
+  halfdelay(10);
+  int ch = 0;
+  while(ch != KB_X)
   {
-    for (int j = 0; j < 21; j++)
+    ch = custom_getch_tick();
+    if(ch == KB_D)
+      mapControl.snakeChangeDirection(1);
+    else if(ch == KB_A)
+      mapControl.snakeChangeDirection(2);
+    else if (ch == KB_S)
+      mapControl.snakeChangeDirection(3);
+    else if (ch == KB_W)
+      mapControl.snakeChangeDirection(4);
+    mapControl.gameTimeFlow();
+    for (int h = 0; h < 21; h++)
     {
-      if(mapControl.getCurrentMapPosition(i,j) == 0)
+      for (int w = 0; w < 21; w++)
       {
-        mvwprintw(gameScreen,j,2*i," ");
-      }
-      else if(mapControl.getCurrentMapPosition(i,j) == 1 || mapControl.getCurrentMapPosition(i,j) == 2)
-      {
-        mvwprintw(gameScreen,j,2*i,"\u25A1");
-      }
-      else if(mapControl.getCurrentMapPosition(i,j) == 3)
-      {
-        mvwprintw(gameScreen,j,2*i,"\u25CC");
-      }
-      else if(mapControl.getCurrentMapPosition(i,j) == 4)
-      {
-        mvwprintw(gameScreen,j,2*i,"\u25CB");
+        if(mapControl.getCurrentMapPosition(h,w) == 0)
+        {
+          mvwprintw(gameScreen,h,2*w," ");
+        }
+        else if(mapControl.getCurrentMapPosition(h,w) == 1 || mapControl.getCurrentMapPosition(h,w) == 2)
+        {
+          mvwprintw(gameScreen,h,2*w,"\u25A1");
+        }
+        else if(mapControl.getCurrentMapPosition(h,w) == 3)
+        {
+          mvwprintw(gameScreen,h,2*w,"\u25CC");
+        }
+        else if(mapControl.getCurrentMapPosition(h,w) == 4)
+        {
+          mvwprintw(gameScreen,h,2*w,"\u25CB");
+        }
       }
     }
-  }
   wrefresh(gameScreen);
+  }
+
 
 
   //윈도우 정리
